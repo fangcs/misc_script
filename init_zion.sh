967a52b99a4680e3ad6e5698f3404adbe5412508
fanglu <fanglupku@gmail.com>
2015-02-26 21:44:51 -0800

    add init script for zion cluster

---
+++ init_zion.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+nodename=("zion-1" "zion-2" "zion-3" "zion-4" "zion-5" "zion-6" "zion-7" "zion-8" "zion-9" "zion-10" "zion-11" "zion-12")
+
+for thisnode in "${nodename[@]}"
+do
+	echo "processing node: $thisnode"
+	ssh $thisnode 'mkdir -p /scratch/lfang/hadoop26'
+done
